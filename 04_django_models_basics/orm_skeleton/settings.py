c5ccfbbef4fec529307521e77e80f0b53b96f9b3
wako8o <emilaleksandrov11@gmail.com>
2025-11-02 15:58:10 +0100

    new

--- 04_django_models_basics/orm_skeleton/settings.py
+++ 04_django_models_basics/orm_skeleton/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '**********************************************+'
+SECRET_KEY = '****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,9 +73,9 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
      "default": {
          "ENGINE": "django.db.backends.postgresql",
-         "NAME": "*****",
-         "USER": "*****",
-         "PASSWORD": "*****",
+         "NAME": "***", # your database name
+         "USER": "***", # your database user
+         "PASSWORD": "***", # your database password
          "HOST": "localhost",
          "PORT": "5432",
      }
