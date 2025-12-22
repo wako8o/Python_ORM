8f353b927b76b5edfa683025356674724dbd38d0
wako8o <emilaleksandrov11@gmail.com>
2025-12-22 14:41:30 +0100

    new

--- 13_exercise_django_models_relations/orm_skeleton/settings.py
+++ 13_exercise_django_models_relations/orm_skeleton/settings.py
@@ -65,9 +65,9 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'orm_13',
-        'USER': 'emil',
-        'PASSWORD': 'pgadmin',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
