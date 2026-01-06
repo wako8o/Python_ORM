062179258a2daebf7da40537da94f91db5ba77ab
wako8o <emilaleksandrov11@gmail.com>
2026-01-06 14:12:59 +0100

    new

--- 15_exercise_models_inheritance_and_customization/caller.py
+++ 15_exercise_models_inheritance_and_customization/caller.py
@@ -1,5 +1,27 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm_solution.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm_skeleton.settings')
 django.setup()
+
+from main_app.models import Mage, Necromancer
+
+
+mage = Mage.objects.filter(
+    name="Fire Mage",
+    description="A powerful mage specializing in fire magic.",
+    elemental_power="Fire",
+    spellbook_type="Ancient Grimoire"
+).first()
+
+necromancer = Necromancer.objects.filter(
+    name="Dark Necromancer",
+    description="A mage specializing in dark necromancy.",
+    elemental_power="Darkness", spellbook_type="Necronomicon",
+    raise_dead_ability="Raise Undead Army"
+).first()
+print(mage.elemental_power)
+print(mage.spellbook_type)
+print(necromancer.name)
+print(necromancer.description)
+print(necromancer.raise_dead_ability)
