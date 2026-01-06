4dd2a912dcb8a5fa2bbf4135f7b0f365b718b188
wako8o <emilaleksandrov11@gmail.com>
2026-01-06 15:50:59 +0100

    new

--- 15_exercise_models_inheritance_and_customization/main_app/models.py
+++ 15_exercise_models_inheritance_and_customization/main_app/models.py
@@ -42,11 +42,39 @@ class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
 
 
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
 
+class Message(models.Model):
+    sender = models.ForeignKey(to= UserProfile,related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(to=UserProfile, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
+    def mark_as_read(self):
+        self.is_read = True
 
+    def reply_to_message(self, reply_content: str):
+        new_message = Message(
+            sender=self.receiver,
+            receiver=self.sender,
+            content=reply_content
+        )
+        new_message.save()
+        return new_message
 
+    def forward_message(self, content: UserProfile):
 
+        new_message = Message(
+            sender=self.receiver,
+            receiver=content,
+            content=self.content
+        )
+        new_message.save()
+        return new_message
 
 
 
