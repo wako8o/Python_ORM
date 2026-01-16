987c84a497ee4b0af6c6c87d31bb8e7fa68aa21f
wako8o <emilaleksandrov11@gmail.com>
2026-01-16 12:27:25 +0100

    new

--- 16_advanced_django_model_techniques/main_app/models.py
+++ 16_advanced_django_model_techniques/main_app/models.py
@@ -72,3 +72,19 @@ class FoodCriticRestaurantReview(RestaurantReview):
     class Meta(RestaurantReview.Meta):
         verbose_name = 'Food Critic Review'
         verbose_name_plural = 'Food Critic Reviews'
+
+
+class MenuReview(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    menu = models.ForeignKey(to=Menu, on_delete=models.CASCADE)
+    review_content = models.TextField()
+    rating = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(5)
+    ])
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = ['reviewer_name', 'menu']
+        indexes = [models.Index(fields=['menu'], name='main_app_menu_review_menu_id')]
