d922c06b7d99a82981656141d96e5ab50a4fb3f9
wako8o <emilaleksandrov11@gmail.com>
2026-01-30 12:56:43 +0100

    new

---
+++ 18_advanced_queries_in_django/caller.py
@@ -0,0 +1,13 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models
+from main_app.models import Product, Category, Customer, Order, OrderProduct
+
+
+
+
